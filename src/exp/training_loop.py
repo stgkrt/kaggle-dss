575a9ec83244040626be6131705cade0527ea305
stgkrt <taro.stst@gmail.com>
2023-10-01 15:08:37 +0000

    feat:construction: (#1) implementing train loop before implementing scoring fn

--- src/exp/training_loop.py
+++ src/exp/training_loop.py
@@ -2,10 +2,13 @@ import gc
 import os
 import sys
 import time
+import warnings
 
 import numpy as np
 import pandas as pd  # type: ignore
 import torch
+
+warnings.filterwarnings("ignore")
 
 SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(os.path.join(SRC_DIR, "dss_utils"))
@@ -20,7 +23,7 @@ from scheduler import get_optimizer, get_scheduler
 from train_valid_split import get_train_valid_key_df, get_train_valid_series_df
 
 
-def train_fn(train_loader, model, criterion, epoch, optimizer, LOGGER, CFG):
+def train_fn(CFG, epoch, model, train_loader, criterion, optimizer, LOGGER):
     model.train()
     prog_loagger = ProgressLogger(
         data_num=len(train_loader),
@@ -63,7 +66,14 @@ def get_validation_pred_target(
     return test_targets, test_preds
 
 
-def valid_fn(model, valid_loader, LOGGER, CFG):
+def valid_fn(
+    CFG,
+    epoch,
+    model,
+    valid_loader,
+    criterion,
+    LOGGER,
+):
     model.eval()
     test_targets = np.empty(0)
     test_preds = np.empty(0)
@@ -81,8 +91,9 @@ def valid_fn(model, valid_loader, LOGGER, CFG):
         with torch.no_grad():
             preds = model(inputs)
             preds = torch.sigmoid(preds)
-
-        prog_loagger.log_progress(batch_idx, losses)
+            loss = criterion(preds, targets)
+        losses.update(loss.item(), CFG.batch_size)
+        prog_loagger.log_progress(epoch, batch_idx, losses)
         test_targets, test_preds = get_validation_pred_target(
             targets, preds, test_targets, test_preds
         )
@@ -123,10 +134,15 @@ def training_loop(CFG, LOGGER):
         for epoch in range(0, CFG.n_epoch):
             LOGGER.info(f"- epoch:{epoch} -")
             train_loss_avg = train_fn(
-                train_loader, model, criterion, epoch, optimizer, LOGGER, CFG
+                CFG, epoch, model, train_loader, criterion, optimizer, LOGGER
             )
             valid_targets, valid_preds, valid_loss_avg = valid_fn(
-                model, valid_loader, LOGGER, CFG, criterion
+                CFG,
+                epoch,
+                model,
+                valid_loader,
+                criterion,
+                LOGGER,
             )
             # auc = calc_auc(valid_targets, valid_preds)
             # score, threshold, _ = calc_cv(valid_targets, valid_preds)
@@ -134,11 +150,10 @@ def training_loop(CFG, LOGGER):
             scheduler.step()
 
             elapsed = int(time.time() - start_time) / 60
-            LOGGER.info(f"\t epoch:{epoch}, fold:{fold}")
-            LOGGER.info(
-                f"\t loss train:{train_loss_avg:.4f}, valid:{valid_loss_avg:.4f}"
-            )
-            LOGGER.info(f"\t lr:{lr:.6f}, elapsed time:{elapsed:.2f} min")
+            log_str = f"Epoch:{epoch}, fold:{fold}"
+            log_str += f", train:{train_loss_avg:.4f}, valid:{valid_loss_avg:.4f}"
+            log_str += f", lr:{lr:.6f}, elapsed time:{elapsed:.2f} min"
+            LOGGER.info(log_str)
             # competition scoreを計算するように変更する
 
         del model, train_loader, valid_loader
@@ -158,20 +173,28 @@ if __name__ == "__main__":
             os.path.join(
                 ROOT_DIR,
                 "input",
-                "google-research-identify-contrails-reduce-global-warming",
             )
         )
+        COMPETITION_DIR = os.path.join(
+            INPUT_DIR,
+            "child-mind-institute-detect-sleep-states",
+        )
         OUTPUT_DIR = os.path.abspath(os.path.join(ROOT_DIR, "working", "debug"))
-        TRAIN_DIR = os.path.join(INPUT_DIR, "train")
-        TEST_DIR = os.path.join(INPUT_DIR, "test")
-        df_path = os.path.join(INPUT_DIR, "train_df_fold.csv")
+        # TRAIN_DIR = os.path.join(COMPETITION_DIR, "train")
+        # TEST_DIR = os.path.join(COMPETITION_DIR, "test")
+        key_df = os.path.join(INPUT_DIR, "datakey_unique_non_null.csv")
+        series_df = os.path.join(INPUT_DIR, "train_series_withkey_non_null.parquet")
         # data
         folds = [0]
-        num_workers = 2
+        n_folds = 5
+        num_workers = os.cpu_count()
+        seed = 42
+        group_key = "series_id"
 
         # model
-        inp_channels = 2
-        out_channels = 1
+        input_channels = 2
+        output_channels = 1
+        embedding_base_channels = 16
 
         # training
         n_epoch = 1
@@ -192,6 +215,4 @@ if __name__ == "__main__":
 
     LOGGER = init_logger(log_file=os.path.join(CFG.OUTPUT_DIR, "check.log"))
     LOGGER.info(f"using device: {CFG.device}")
-    score_list, threshold_list, epoch_list, auc_list, auc_epoch_list = training_loop(
-        CFG, LOGGER
-    )
+    training_loop(CFG, LOGGER)
