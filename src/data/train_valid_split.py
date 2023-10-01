673510ab36d524dd3d62dc1dfef4f123e95b1cec
stgkrt <taro.stst@gmail.com>
2023-10-01 14:10:59 +0000

    update:hammer: (#1) train valid split fn

--- src/data/train_valid_split.py
+++ src/data/train_valid_split.py
@@ -11,16 +11,17 @@ def get_group_groupkfold_split(df, CFG):
     return df
 
 
-def get_train_valid_df(df, fold, CFG):
+def get_train_valid_key_df(df, fold, CFG):
     df = get_group_groupkfold_split(df, n_splits=CFG.n_fold, seed=CFG.seed)
     train_df = df[df["fold"] != fold].reset_index(drop=True)
     valid_df = df[df["fold"] == fold].reset_index(drop=True)
     return train_df, valid_df
 
 
-def get_train_valid_series_df(series_df, key_df, fold):
-    train_series_ids = key_df[key_df["fold"] != fold]["series_id"].unique()
-    valid_series_ids = key_df[key_df["fold"] == fold]["series_id"].unique()
-    train_series_df = series_df[series_df["series_id"].isin(train_series_ids)]
-    valid_series_df = series_df[series_df["series_id"].isin(valid_series_ids)]
-    return train_series_df, valid_series_df
+def get_train_valid_series_df(series_df, key_df, fold, mode="train"):
+    if mode == "train":
+        series_ids = key_df[key_df["fold"] != fold]["series_id"].unique()
+    elif mode == "valid":
+        series_ids = key_df[key_df["fold"] == fold]["series_id"].unique()
+    series_df = series_df[series_df["series_id"].isin(series_ids)]
+    return series_df
