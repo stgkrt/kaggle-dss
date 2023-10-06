6008eade77ed30bc7bb8ff3043150b5f176c892e
stgkrt <taro.stst@gmail.com>
2023-10-06 16:28:08 +0000

    feat:construction:(#1) create oof df but step is wrong

--- src/exp/training_loop.py
+++ src/exp/training_loop.py
@@ -34,7 +34,7 @@ def train_fn(CFG, epoch, model, train_loader, criterion, optimizer, LOGGER):
     event_losses = AverageMeter()
 
     losses = AverageMeter()
-    for batch_idx, (inputs, class_targets, event_targets) in enumerate(train_loader):
+    for batch_idx, (inputs, class_targets, event_targets, _) in enumerate(train_loader):
         inputs = inputs.to(CFG.device, non_blocking=True).float()
         class_targets = class_targets.to(CFG.device, non_blocking=True).float()
         event_targets = event_targets.to(CFG.device, non_blocking=True).float()
@@ -89,20 +89,44 @@ def get_validation_pred_target(
     return test_class_preds, test_event_preds, test_class_targets, test_event_targets
 
 
-def valid_fn(
-    CFG,
-    epoch,
-    model,
-    valid_loader,
-    criterion,
-    LOGGER,
-):
+def get_valid_values_dict(
+    class_values: torch.Tensor,
+    event_values: torch.Tensor,
+    validation_dict: dict,
+    mode: str = "preds",
+) -> dict:
+    class_values = class_values.detach().cpu().numpy()
+    event_values = event_values.detach().cpu().numpy()
+    if len(validation_dict[f"class_{mode}"]) == 0:
+        validation_dict[f"class_{mode}"] = class_values
+        validation_dict[f"event_{mode}"] = event_values
+    else:
+        validation_dict[f"class_{mode}"] = np.concatenate(
+            [validation_dict[f"class_{mode}"], class_values], axis=0
+        )
+        validation_dict[f"event_{mode}"] = np.concatenate(
+            [validation_dict[f"event_{mode}"], event_values], axis=0
+        )
+    return validation_dict
+
+
+def concat_valid_input_info(valid_input_info: dict, input_info: dict) -> dict:
+    if len(valid_input_info["series_date_key"]) == 0:
+        valid_input_info["series_date_key"] = input_info["series_date_key"]
+        valid_input_info["start_step"] = input_info["start_step"]
+    else:
+        valid_input_info["series_date_key"] = np.concatenate(
+            [valid_input_info["series_date_key"], input_info["series_date_key"]], axis=0
+        )
+        valid_input_info["start_step"] = np.concatenate(
+            [valid_input_info["start_step"], input_info["start_step"]], axis=0
+        )
+    return valid_input_info
+
+
+def valid_fn(CFG, epoch, model, valid_loader, criterion, LOGGER):
     model.eval()
 
-    test_class_preds = np.empty(0)
-    test_event_preds = np.empty(0)
-    test_class_targets = np.empty(0)
-    test_event_targets = np.empty(0)
     losses = AverageMeter()
     class_losses = AverageMeter()
     event_losses = AverageMeter()
@@ -112,8 +136,13 @@ def valid_fn(
         logger=LOGGER,
         mode="valid",
     )
+    valid_predictions = {"class_preds": np.empty(0), "event_preds": np.empty(0)}
+    valid_targets = {"class_targets": np.empty(0), "event_targets": np.empty(0)}
+    valid_input_info = {"series_date_key": [], "start_step": []}
 
-    for batch_idx, (inputs, class_targets, event_targets) in enumerate(valid_loader):
+    for batch_idx, (inputs, class_targets, event_targets, input_info_dict) in enumerate(
+        valid_loader
+    ):
         inputs = inputs.to(CFG.device, non_blocking=True).float()
         class_targets = class_targets.to(CFG.device, non_blocking=True).float()
         event_targets = event_targets.to(CFG.device, non_blocking=True).float()
@@ -127,37 +156,113 @@ def valid_fn(
         class_losses.update(class_loss.item(), CFG.batch_size)
         event_losses.update(event_loss.item(), CFG.batch_size)
         prog_loagger.log_progress(epoch, batch_idx, class_losses, event_losses)
-        (
-            test_class_preds,
-            test_event_preds,
-            test_class_targets,
-            test_event_targets,
-        ) = get_validation_pred_target(
-            class_preds,
-            event_preds,
-            class_targets,
-            event_targets,
-            test_class_preds,
-            test_event_preds,
-            test_class_targets,
-            test_event_targets,
+
+        valid_predictions = get_valid_values_dict(
+            class_preds, event_preds, valid_predictions, mode="preds"
         )
+        valid_targets = get_valid_values_dict(
+            class_targets, event_targets, valid_targets, mode="targets"
+        )
+        valid_input_info = concat_valid_input_info(valid_input_info, input_info_dict)
 
         del inputs, class_preds, event_preds, class_targets, event_targets
         gc.collect()
         torch.cuda.empty_cache()
     return (
-        test_class_preds,
-        test_event_preds,
-        test_class_targets,
-        test_event_targets,
+        valid_predictions,
+        valid_targets,
+        valid_input_info,
         losses.avg,
     )
+
+
+def get_oof_df(
+    valid_input_info_dict: dict,
+    valid_preds_dict: dict,
+    valid_targets_dict: dict,
+    oof_df: pd.DataFrame,
+    oof_df_fold: pd.DataFrame,
+) -> pd.DataFrame:
+    start_time = time.time()
+    print("create oof_df", end=" ... ")
+    for idx, (series_date_key, start_step) in enumerate(
+        zip(
+            valid_input_info_dict["series_date_key"],
+            valid_input_info_dict["start_step"],
+        )
+    ):
+        # preds targets shape: [batch, ch, data_length]
+        class_pred = valid_preds_dict["class_preds"][idx]
+        event_pred = valid_preds_dict["event_preds"][idx]
+        class_target = valid_targets_dict["class_targets"][idx]
+        event_target = valid_targets_dict["event_targets"][idx]
+
+        series_date_data_num = len(
+            oof_df_fold[oof_df_fold["series_date_key"] == series_date_key]
+        )
+        steps = range(start_step, start_step + series_date_data_num, 1)
+        if series_date_data_num < len(class_pred[0]):
+            class_pred = class_pred[0, :series_date_data_num]
+            event_onset_pred = event_pred[0, :series_date_data_num]
+            event_wakeup_pred = event_pred[1, :series_date_data_num]
+            class_target = class_target[0, :series_date_data_num]
+            event_onset_target = event_target[0, :series_date_data_num]
+            event_wakeup_target = event_target[1, :series_date_data_num]
+        elif series_date_data_num > len(class_pred[0]):
+            padding_num = series_date_data_num - len(class_pred[0])
+            class_pred = np.concatenate(
+                [class_pred[0], -1 * np.ones(padding_num)], axis=0
+            )
+            event_onset_pred = np.concatenate(
+                [event_pred[0], -1 * np.ones(padding_num)], axis=0
+            )
+            event_wakeup_pred = np.concatenate(
+                [event_pred[1], -1 * np.ones(padding_num)], axis=0
+            )
+            class_target = np.concatenate(
+                [class_target[0], -1 * np.ones(padding_num)], axis=0
+            )
+            event_onset_target = np.concatenate(
+                [event_target[0], -1 * np.ones(padding_num)], axis=0
+            )
+            event_wakeup_target = np.concatenate(
+                [event_target[1], -1 * np.ones(padding_num)], axis=0
+            )
+
+        else:
+            class_pred = class_pred[0]
+            event_onset_pred = event_pred[0]
+            event_wakeup_pred = event_pred[1]
+            class_target = class_target[0]
+            event_onset_target = event_target[0]
+            event_wakeup_target = event_target[1]
+
+        oof_df_fold.loc[
+            (oof_df_fold["series_date_key"] == series_date_key)
+        ] = oof_df_fold.loc[(oof_df_fold["series_date_key"] == series_date_key)].assign(
+            class_pred=class_pred,
+            event_onset_pred=event_onset_pred,
+            event_wakeup_pred=event_wakeup_pred,
+            class_target=class_target,
+            event_onset_target=event_onset_target,
+            event_wakeup_target=event_wakeup_target,
+            steps=steps,
+        )
+
+    if len(oof_df) == 0:
+        oof_df = oof_df_fold.copy()
+    else:
+        oof_df = pd.concat([oof_df, oof_df_fold], axis=0)
+
+    elapsed = int(time.time() - start_time) / 60
+    print(f" >> oof_df created. elapsed time: {elapsed:.2f} min")
+    return oof_df
 
 
 def training_loop(CFG, LOGGER):
     key_df = pd.read_csv(CFG.key_df)
     series_df = pd.read_parquet(CFG.series_df)
+    oof_df = pd.DataFrame()
     for fold in CFG.folds:
         LOGGER.info(f"-- fold{fold} training start --")
         # set model & learning fn
@@ -182,16 +287,29 @@ def training_loop(CFG, LOGGER):
 
         train_loader = get_loader(CFG, train_key_df, train_series_df, mode="train")
         valid_loader = get_loader(CFG, valid_key_df, valid_series_df, mode="valid")
+
+        oof_df_fold = valid_series_df.copy()
+        init_cols = [
+            "class_pred",
+            "event_onset_pred",
+            "event_wakeup_pred",
+            "class_target",
+            "event_onset_target",
+            "event_wakeup_target",
+        ]
+        oof_df_fold = oof_df_fold.assign(
+            **{col: -1 * np.ones(len(oof_df_fold)) for col in init_cols}
+        )
+
         for epoch in range(0, CFG.n_epoch):
             LOGGER.info(f"- epoch:{epoch} -")
             train_loss_avg = train_fn(
                 CFG, epoch, model, train_loader, criterion, optimizer, LOGGER
             )
             (
-                valid_class_preds,
-                valid_event_preds,
-                valid_class_targets,
-                valid_event_targets,
+                valid_predictions,
+                valid_targets,
+                input_info_dict_list,
                 valid_loss_avg,
             ) = valid_fn(
                 CFG,
@@ -200,6 +318,13 @@ def training_loop(CFG, LOGGER):
                 valid_loader,
                 criterion,
                 LOGGER,
+            )
+            oof_df = get_oof_df(
+                input_info_dict_list,
+                valid_predictions,
+                valid_targets,
+                oof_df,
+                oof_df_fold,
             )
             # auc = calc_auc(valid_targets, valid_preds)
             # score, threshold, _ = calc_cv(valid_targets, valid_preds)
@@ -216,6 +341,12 @@ def training_loop(CFG, LOGGER):
         del model, train_loader, valid_loader
         gc.collect()
         torch.cuda.empty_cache()
+
+    oof_df_path = os.path.join(CFG.OUTPUT_DIR, "oof_df.parquet")
+    print("save oof_df to ", oof_df_path)
+    oof_df.to_parquet(oof_df_path)
+    print("oof_df saved. finish exp.")
+    # return oof_df
 
 
 if __name__ == "__main__":
@@ -264,7 +395,8 @@ if __name__ == "__main__":
         lr = 1e-4
         weight_decay = 1e-6
         # scheduler
-        T_0 = 10
+        # T_0 = 10
+        T_0 = n_epoch
         T_mult = 1
         eta_min = 1e-9
 
