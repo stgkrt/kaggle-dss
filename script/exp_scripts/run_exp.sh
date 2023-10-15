8b1bb112a3e890e70473c9df921e4fe76683af79
stgkrt <taro.stst@gmail.com>
2023-10-15 22:39:39 +0000

    update:hammer: (#8) parser setting

--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -1,7 +1,10 @@
 
 
-python src/exp/run_exp.py --exp_category build_baseline \
-                        --exp_name baseline000 \
+python src/exp/run_exp.py --exp_category build_baseline_alldata \
+                        --exp_name baseline_alldata_allfold_000 \
+                        --model_type add_rolldiff \
+                        --input_channels 4 \
+                        --folds 0 1 2 3 4 \
                         --n_epoch 10 \
                         --T_0 10
 
