3a421ab59e6a066f0e4c549f6d86ad57a09e809e
stgkrt <taro.stst@gmail.com>
2023-10-24 15:52:46 +0000

    update:hammer: exp script

--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -3,18 +3,18 @@ epoch=10
 
 # python src/exp/run_exp.py --exp_category baseline \
 #                         --exp_name exp007_addch \
-#                         --input_channels 6 \
+#                         --input_channels 8 \
 #                         --folds 0 1 2 3 4 \
 #                         --n_epoch $epoch \
 #                         --T_0 $epoch \
 #                         --lr 0.001 \
 
 python src/exp/run_exp.py --exp_category baseline_fold0 \
-                        --exp_name exp007_addch \
-                        --input_channels 6 \
+                        --exp_name exp007_addch_fold0 \
+                        --input_channels 8 \
                         --folds 0 \
                         --n_epoch $epoch \
                         --T_0 $epoch \
-                        --lr 0.0001
+                        --lr 0.001
 
 wandb sync --sync-all
