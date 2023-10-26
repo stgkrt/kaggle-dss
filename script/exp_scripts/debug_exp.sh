e06d1e1550ec779ed57093c2b3288dc7d22885bd
stgkrt <taro.stst@gmail.com>
2023-10-26 14:32:09 +0000

    update:hammer: exp scripts

--- script/exp_scripts/debug_exp.sh
+++ script/exp_scripts/debug_exp.sh
@@ -1,8 +1,9 @@
 
 python src/exp/run_exp.py --exp_name debug \
-                        --folds 0 1 2 3 4 \
+                        --folds 0 \
                         --n_epoch 1 \
-                        --input_channels 4
+                        --input_channels 14 \
+                        --model_type mean_stds
 
 
 # python src/exp/run_exp.py  --train-mode pseudo \
--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -2,16 +2,17 @@
 epoch=10
 
 python src/exp/run_exp.py --exp_category baseline \
-                        --exp_name exp007_addch \
-                        --input_channels 8 \
+                        --exp_name exp008_meanstd_nonavepool \
+                        --input_channels 14 \
                         --folds 0 1 2 3 4 \
                         --n_epoch $epoch \
                         --T_0 $epoch \
-                        --lr 0.001 \
+                        --lr 0.001
 
 # python src/exp/run_exp.py --exp_category baseline_fold0 \
-#                         --exp_name exp007_addch_fold0 \
-#                         --input_channels 8 \
+#                         --exp_name exp008_meanstd_noavepool_fold0 \
+#                         --input_channels 14 \
+#                         --model_type mean_stds \
 #                         --folds 0 \
 #                         --n_epoch $epoch \
 #                         --T_0 $epoch \
