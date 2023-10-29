541670f258339c614f786bcbf5ae6b735f3e7ec0
stgkrt <taro.stst@gmail.com>
2023-10-29 05:29:27 +0000

    update:hammer: exp scripts

--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -1,5 +1,13 @@
 
 epoch=10
+python src/exp/run_exp.py --exp_category baseline \
+                        --exp_name exp009_normal \
+                        --model_type normal \
+                        --input_channels 2 \
+                        --folds 0 1 2 3 4 \
+                        --n_epoch $epoch \
+                        --T_0 $epoch \
+                        --lr 0.001
 
 # python src/exp/run_exp.py --exp_category baseline \
 #                         --exp_name exp008_meanstd_nonavepool \
