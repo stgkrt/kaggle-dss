01f37488b2c4256c342dade815426c61df19722d
stgkrt <taro.stst@gmail.com>
2023-10-30 14:42:07 +0000

    update:hammer: exp scripts

--- script/exp_scripts/debug_exp.sh
+++ script/exp_scripts/debug_exp.sh
@@ -5,19 +5,26 @@
 #                         --input_channels 2 \
 #                         --model_type normal
 
+python src/exp/run_exp.py --exp_name debug \
+                        --folds 0 \
+                        --n_epoch 1 \
+                        --input_channels 14 \
+                        --model_type mean_stds \
+                        --folds 0 \
+                        --lr 0.001
 
 
 # python src/exp/run_exp.py  --train-mode pseudo \
 #                             --exp_name pseudo_debug
 
-oof_df_path="/kaggle/working/_oof/exp008_meanstd_nonavepool/oof_df_secondstg.parquet"
-python src/exp/run_exp.py --exp_category debug_secondstage \
-                        --exp_name debug_secondstg \
-                        --model_type event_detect \
-                        --input_channels 3 \
-                        --output_channels 2 \
-                        --series_df $oof_df_path \
-                        --folds 0  \
-                        --n_epoch 5 \
-                        --T_0 1 \
-                        --lr 0.001
+# oof_df_path="/kaggle/working/_oof/exp008_meanstd_nonavepool/oof_df_secondstg.parquet"
+# python src/exp/run_exp.py --exp_category debug_secondstage \
+#                         --exp_name debug_secondstg \
+#                         --model_type event_detect \
+#                         --input_channels 3 \
+#                         --output_channels 2 \
+#                         --series_df $oof_df_path \
+#                         --folds 0  \
+#                         --n_epoch 5 \
+#                         --T_0 1 \
+#                         --lr 0.001
--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -1,32 +1,32 @@
 
 epoch=10
 
-oof_df_path="/kaggle/working/_oof/exp008_meanstd_nonavepool/oof_df_secondstg.parquet"
-for epoch in 50 30 10
-    do
-    python src/exp/run_exp.py --exp_category secondstage_baseline \
-                            --exp_name secondstg_making_epoch${epoch} \
-                            --model_type event_detect \
-                            --input_channels 3 \
-                            --output_channels 2 \
-                            --series_df $oof_df_path \
-                            --folds 0  \
-                            --n_epoch 5 \
-                            --T_0 1 \
-                            --lr 0.001
-    done
+# oof_df_path="/kaggle/working/_oof/exp008_meanstd_nonavepool/oof_df_secondstg.parquet"
+# for epoch in 50 30 20 10
+#     do
+#     python src/exp/run_exp.py --exp_category secondstage_baseline \
+#                             --exp_name secondstg_making_epoch${epoch} \
+#                             --model_type event_detect \
+#                             --input_channels 3 \
+#                             --output_channels 2 \
+#                             --series_df $oof_df_path \
+#                             --folds 0  \
+#                             --n_epoch $epoch \
+#                             --T_0 1 \
+#                             --lr 0.001
+#     done
 
-# python src/exp/run_exp.py --exp_category baseline \
-#                         --exp_name exp008_meanstd_nonavepool \
-#                         --model_type mean_stds \
-#                         --input_channels 14 \
-#                         --folds 0 1 2 3 4 \
-#                         --n_epoch $epoch \
-#                         --T_0 $epoch \
-#                         --lr 0.001
+python src/exp/run_exp.py --exp_category baseline \
+                        --exp_name exp010_meanstd_norminput \
+                        --model_type mean_stds \
+                        --input_channels 14 \
+                        --folds 0 1 2 3 4 \
+                        --n_epoch $epoch \
+                        --T_0 $epoch \
+                        --lr 0.001
 
 # python src/exp/run_exp.py --exp_category baseline_fold0 \
-#                         --exp_name exp008_meanstd_noavepool_fold0 \
+#                         --exp_name exp010_meanstd_norminput_fold0 \
 #                         --input_channels 14 \
 #                         --model_type mean_stds \
 #                         --folds 0 \
