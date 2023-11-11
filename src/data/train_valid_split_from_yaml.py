73c9d5de9d6c7b206fb268dbf695759871cbd5a4
stgkrt <taro.stst@gmail.com>
2023-11-11 02:41:06 +0000

    feat:sparkles: skf fold split

---
+++ src/data/train_valid_split_from_yaml.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import yaml
+
+
+def set_fold_from_yaml(CFG, series_df, yaml_path_list):
+    series_df["fold"] = -1
+    for fold, fold_yaml_path in enumerate(CFG.fold_yamls):
+        with open(fold_yaml_path, "r") as f:
+            fold_series_dict = yaml.load(f, Loader=yaml.FullLoader)
+        print(
+            len(fold_series_dict["train_series_ids"]),
+            len(fold_series_dict["valid_series_ids"]),
+        )
+
+        series_df.loc[
+            series_df["series_id"].isin(fold_series_dict["valid_series_ids"]), "fold"
+        ] = fold
+        print(len(series_df[series_df["fold"] == fold]["series_id"].unique()))
+    return series_df
+
+
+if __name__ == "__main__":
+
+    class CFG:
+        n_folds = 5
+        group_key = "series_id"
+
+        filename = "/kaggle/input/targetdownsample_train_series_hour.parquet"
+        output_filename = "/kaggle/input/targetdownsample_train_series_skffold.parquet"
+        fold_yamls = [
+            f"/kaggle/input/fold_split_yaml/stfk_event_count_bins105_fold{i}.yaml"
+            for i in range(n_folds)
+        ]
+
+    series_df = pd.read_parquet(CFG.filename)
+    series_df = set_fold_from_yaml(CFG, series_df, CFG.fold_yamls)
+    series_df.to_parquet(CFG.output_filename)
