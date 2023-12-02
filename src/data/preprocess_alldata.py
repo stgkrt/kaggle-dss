071174e6f719f5bf045e7f0238663ca1139e118f
stgkrt <taro.stst@gmail.com>
2023-12-02 15:38:07 +0000

    feat:sparkles: duplicate and elapsed date feature

--- src/data/preprocess_alldata.py
+++ src/data/preprocess_alldata.py
@@ -1,15 +1,18 @@
+import datetime
 import warnings
 
+import numpy as np
 import pandas as pd
 import polars as pl
+from scipy.signal import savgol_filter
 
 warnings.filterwarnings("ignore")
 
 
 def pl_datetime_preprocess(train_series_):
-    train_series_ = train_series_.with_columns(
-        pl.col("timestamp").str.to_datetime().dt.replace_time_zone(None)
-    )
+    # train_series_ = train_series_.with_columns(
+    #     pl.col("timestamp").str.to_datetime().dt.replace_time_zone(None)
+    # )
     train_series_ = train_series_.with_columns(
         pl.col("timestamp").dt.second().cast(pl.Int32).alias("second")
     )
@@ -17,6 +20,104 @@ def pl_datetime_preprocess(train_series_):
         pl.col("timestamp").dt.date().cast(str).alias("date")
     )
     return train_series_
+
+
+def add_elapsed_date(df: pl.DataFrame):
+    # series_idごとにmin, maxを取得
+    df = df.with_columns(
+        pl.col("timestamp").str.to_datetime().dt.replace_time_zone(None)
+    )
+    min_time_df = (
+        df.select(["series_id", "timestamp"])
+        .groupby("series_id", maintain_order=True)
+        .min()
+    )
+    unique_series_id = df["series_id"].unique()
+    add_elapsed_date_list = []
+    for series_id in unique_series_id:
+        first_date = min_time_df.filter(pl.col("series_id") == series_id).get_column(
+            "timestamp"
+        )[0]
+        elaped_date = (
+            df.filter(pl.col("series_id") == series_id).get_column("timestamp")
+            - first_date
+        )
+        elaped_date = elaped_date.dt.days()
+        # elpaed dateを最大値で割る
+        normalized_elapsed_date = elaped_date / elaped_date.max()
+        add_elapsed_date_list.extend(normalized_elapsed_date)
+    df = df.with_columns(pl.Series(add_elapsed_date_list).alias("elapsed_date"))
+    return df
+
+
+def pl_datetime_preprocess_hourshift(train_series_, shift_hour=12):
+    train_series_ = train_series_.with_columns(
+        pl.col("timestamp").str.to_datetime().dt.replace_time_zone(None)
+    )
+    if shift_hour != 0:
+        train_series_ = train_series_.with_columns(
+            pl.col("timestamp") + datetime.timedelta(hours=12)
+        )
+    train_series_ = train_series_.with_columns(
+        pl.col("timestamp").dt.second().cast(pl.Int32).alias("second")
+    )
+    train_series_ = train_series_.with_columns(
+        pl.col("timestamp").dt.date().cast(str).alias("date")
+    )
+    return train_series_
+
+
+def add_filter_feature(series_df: pl.DataFrame):
+    series_df = series_df.with_columns(
+        pl.col("anglez").diff(1).abs().alias("anglez_abs_diff")
+    )
+    # anglez_abs_diffのnanを0で埋める
+    series_df = series_df.with_columns(
+        pl.col("anglez_abs_diff").fill_nan(0).alias("anglez_abs_diff")
+    )
+    # series_df["filtered_anglez"] = savgol_filter(series_df["anglez_abs_diff"].copy())
+    series_df = series_df.with_columns(
+        pl.Series("anglez_abs_diff")
+        .apply(savgol_filter, return_dtype=float)
+        .alias("filtered_anglez")
+    )
+    return series_df
+
+
+def add_duplicate(df: pl.DataFrame):
+    # NumPy配列への変換
+    array = df[["enmo", "anglez"]].to_numpy()
+
+    # 180行ごとに分割
+    subsets = [array[i : i + 180] for i in range(0, len(array), 180)]
+
+    subsets_dict = {}
+    for i, subset in enumerate(subsets):
+        # サブセットをタプルのリストに変換
+        subset_key = tuple(map(tuple, subset))
+        if subset_key in subsets_dict:
+            subsets_dict[subset_key].append(i)
+        else:
+            subsets_dict[subset_key] = [i]
+
+    # 完全に一致するサブセットのインデックスペアを探す
+    matching_subsets = []
+    for indices in subsets_dict.values():
+        if len(indices) > 1:
+            matching_subsets.extend(indices)
+
+    duplicate_array = np.zeros(len(df), dtype=int)
+
+    # matching_subsetsに含まれるサブセットに対応する配列の範囲を1に更新
+    for index in matching_subsets:
+        if np.mean(subsets[index][:, 0]) == 0:
+            continue
+        start_row = index * 180
+        end_row = start_row + 180
+        duplicate_array[start_row:end_row] = 1
+
+    df = df.with_columns(pl.Series(duplicate_array).alias("duplicate"))
+    return df
 
 
 def preprocess_input(train_series_: pd.DataFrame) -> pd.DataFrame:
@@ -129,8 +230,15 @@ if __name__ == "__main__":
     train_event_df = pd.read_csv(
         "/kaggle/input/child-mind-institute-detect-sleep-states/train_events.csv"
     )
+    print("add elapsed date")
+    train_series_df = add_elapsed_date(train_series_df)
     print("preprocess polars train series")
     train_series_df = pl_datetime_preprocess(train_series_df)
+    # print("add duplicate")
+    # train_series_df = add_duplicate(train_series_df)
+    # print("add filter feature")
+    # train_series_df = add_filter_feature(train_series_df)
+    # train_series_df = pl_datetime_preprocess_12hourshift(train_series_df)
     train_series_df = train_series_df.to_pandas()
     print(train_series_df.info())
     train_series_df = preprocess_train_series(train_series_df, train_event_df)
@@ -138,4 +246,4 @@ if __name__ == "__main__":
     print(train_series_df.info())
 
     print(len(train_series_df))
-    train_series_df.to_parquet("/kaggle/input/train_series_alldata.parquet")
+    train_series_df.to_parquet("/kaggle/input/train_series_alldata_elapseddate.parquet")
