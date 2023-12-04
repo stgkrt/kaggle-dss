3907bc9e4220155e93aad1df80aacbb1d745c1f8
stgkrt <taro.stst@gmail.com>
2023-12-04 14:29:15 +0000

    update:hammer: exp scripts

--- script/exp_scripts/debug_exp.sh
+++ script/exp_scripts/debug_exp.sh
@@ -1,5 +1,7 @@
-df="/kaggle/input/train_series_alldata_skffold.parquet"
+# df="/kaggle/input/train_series_alldata_skffold.parquet"
 # df="/kaggle/input/train_series_alldata_halflabel_fold.parquet"
+df="/kaggle/input/train_series_alldata_duplicate_fold.parquet"
+# df="/kaggle/input/train_series_alldata_elapseddate_fold.parquet"
 python src/exp/run_exp.py --exp_name debug \
                         --folds 0 \
                         --n_epoch 2 \
@@ -7,7 +9,7 @@ python src/exp/run_exp.py --exp_name debug \
                         --T_0 2 \
                         --series_df $df \
                         --input_channels 6 \
-                        --model_type dense_lstm_enc_head \
+                        --model_type lstm_enc_head_downsample \
                         --lr 0.001
 
                       
--- script/exp_scripts/run_exp.sh
+++ script/exp_scripts/run_exp.sh
@@ -1,30 +1,53 @@
 
-epoch=30
-
-df="/kaggle/input/train_series_alldata_skffold.parquet"
-python src/exp/run_exp.py --exp_category earlysave \
-                        --exp_name exp030_lstm_enchead_enckernelto384_removekeys_trnvld_epoch${epoch} \
-                        --folds 0 1 2 3 4 \
-                        --enc-kernelsize-list 12 24 48 96 192 384 \
-                        --n_epoch $epoch \
-                        --T_0 $epoch \
-                        --series_df $df \
-                        --input_channels 6 \
-                        --model_type dense_lstm_enc_head \
-                        --lr 0.001
-
-wandb sync --sync-all
-
-# df="/kaggle/input/train_series_alldata_halflabel_fold.parquet"
-# python src/exp/run_exp.py --exp_category earlysave \
-#                         --exp_name exp029_lstm_enchead_enckernelto192_halflabel_epoch${epoch} \
+# epoch=15
+# df="/kaggle/input/train_series_alldata_skffold.parquet"
+# df="/kaggle/input/train_series_alldata_elapseddate_fold.parquet"
+# python src/exp/run_exp.py  --exp_category earlysave \
+#                          --exp_name exp037_elapseddate_epoch${epoch} \
 #                         --folds 0 1 2 3 4 \
-#                         --enc-kernelsize-list 12 24 48 96 192 \
 #                         --n_epoch $epoch \
 #                         --T_0 $epoch \
+#                         --enc-kernelsize-list 12 24 48 96 192 384 \
+#                         --series_df $df \
+#                         --input_channels 7 \
+#                         --model_type elapsed_date \
+#                         --lr 0.001
+
+# python src/exp/run_exp.py  --exp_category earlysave \
+#                          --exp_name exp042_epoch${epoch}_fold0\
+#                         --folds 0 \
+#                         --n_epoch $epoch \
+#                         --T_0 $epoch \
+#                         --enc-kernelsize-list 12 24 48 96 192 384 \
 #                         --series_df $df \
 #                         --input_channels 6 \
 #                         --model_type dense_lstm_enc_head \
 #                         --lr 0.001
+epoch=30
+# epoch=10
+# epoch=5
+df="/kaggle/input/train_series_alldata_duplicate_fold.parquet"
+# python src/exp/run_exp.py  --exp_category sleep_onset_wakeup \
+#                          --exp_name exp038_epoch${epoch}_fold0_sigma_tuning \
+#                         --folds 0 \
+#                         --n_epoch $epoch \
+#                         --T_0 $epoch \
+#                         --enc-kernelsize-list 12 24 48 96 192 384 \
+#                         --series_df $df \
+#                         --input_channels 7 \
+#                         --model_type dense3ch \
+#                         --warmup_t 0 \
+#                         --lr 0.001
 
-# wandb sync --sync-all
+python src/exp/run_exp.py  --exp_category earlysave \
+                         --exp_name exp045_randomslide_epoch${epoch} \
+                        --folds 0 1 2 3 4 \
+                        --n_epoch $epoch \
+                        --T_0 $epoch \
+                        --enc-kernelsize-list 12 24 48 96 192 384 \
+                        --series_df $df \
+                        --input_channels 7 \
+                        --model_type dense_lstm_enc_head \
+                        --warmup_t 2 \
+                        --lr 0.001
+wandb sync --sync-all
